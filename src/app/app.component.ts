6db2fb4543a66a4e3fb46b28ce95eb43fecbc0e1
Milton <gmraivan@gmail.com>
2022-12-15 14:36:13 -0500

    test de imagennes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tests';
+export class AppComponent implements OnInit {
+  items = ['item1', 'item2', 'item3', 'item4']
+  ngOnInit(): void {
+
+  }
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+    console.log('index anterior',event.previousIndex)
+    console.log('index nuevo',event.currentIndex)
+  }
+
+
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatListModule } from '@angular/material/list';
+import { ImgComponent } from './components/img/img.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    MatListModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
